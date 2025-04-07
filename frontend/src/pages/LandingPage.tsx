14f78fd526b2e5bb9447b07ea2cd9b3c2dae9d5b
Kate Sargent <katesargentt@gmail.com>
2025-04-07 16:32:05 -0600

    added movie carousel on landing

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -3,25 +3,63 @@ import Header from '../components/Header';
 import { useNavigate } from 'react-router-dom';
 import '../LandingPage.css';
 
-function LandingPage() {
-  const navigate = useNavigate();
 
-  return (
-    <div className="landing-container">
-      <div className="landing-content">
-        <h1 className="landing-title">CineNiche</h1>
-        <div className="button-group">
-          <button className="primary-button" onClick={() => navigate('/createAccount')}>
-            Create Account
-          </button>
-          <button className="secondary-button" onClick={() => navigate('/login')}>
-            Login
-          </button>
-        </div>
-      </div>
-      <Footer />
-    </div>
-  );
+function LandingPage() {
+ const navigate = useNavigate();
+
+
+ return (
+   <div className="landing-container">
+     <div className="landing-content">
+       <h1 className="landing-title">CineNiche</h1>
+       <div className="button-group">
+         <button
+           className="primary-button"
+           onClick={() => navigate('/createAccount')}
+         >
+           Create Account
+         </button>
+         <button
+           className="secondary-button"
+           onClick={() => navigate('/login')}
+         >
+           Login
+         </button>
+       </div>
+       <div className="carousel">
+         <div
+           className="movie-card"
+           style={{ backgroundImage: "url('/images/Boom.jpg')" }}
+         ></div>
+         <div
+           className="movie-card"
+           style={{ backgroundImage: "url('/images/Brain on Fire.jpg')" }}
+         ></div>
+         <div
+           className="movie-card"
+           style={{ backgroundImage: "url('/images/Bright.jpg')" }}
+         ></div>
+         <div
+           className="movie-card"
+           style={{ backgroundImage: "url('/images/Pope Francis A Man of His Word 2.jpg')" }}
+           
+         ></div>
+         <div
+           className="movie-card"
+           style={{ backgroundImage: "url('/images/WE.jpg')" }}
+           
+         ></div>
+         <div
+           className="movie-card"
+           style={{ backgroundImage: "url('/images/American Woman.jpg')" }}
+           
+         ></div>
+       </div>
+     </div>
+     <Footer />
+   </div>
+ );
 }
+
 
 export default LandingPage;
