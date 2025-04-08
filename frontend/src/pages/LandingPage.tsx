09e649f509a0e98c657a3212491ae83f3ff32cff
Kate Sargent <katesargentt@gmail.com>
2025-04-07 19:01:49 -0600

    fixed styling

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,7 +1,7 @@
 import Footer from '../components/Footer';
 //import Header from '../components/Header';
 import { useNavigate } from 'react-router-dom';
-import '../LandingPage.css';
+import './LandingPage.css';
 import CookieConsent from 'react-cookie-consent';
 
 function LandingPage() {
